eb5ef8396a196db6b10ec4845a45acc594f72ba1
globalavocado <globalavocado@gmail.com>
2014-10-01 11:05:12 +0100

    added restriction to no more than five possible ratings, all tests passed

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-# RSpec.describe Review, :type => :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Review, :type => :model do
+	it 'cannot have a rating greater than 5' do
+		restaurant = Restaurant.create(name: "The Ivies")
+		review = restaurant.reviews.create(musings: "Pretty great!", rating: 6)
+		expect(review).to have(1).error_on(:rating)
+	end  
+
+	
+
+end
